0741a470a580f4acc90239dbcd7ae1198b607ae0
yjyj1023 <lyj19961023@gmail.com>
2022-10-20 13:29:47 +0900

    toby spring test코드 count생성

--- src/test/java/org/example/UserDaoTest.java
+++ src/test/java/org/example/UserDaoTest.java
@@ -36,6 +36,27 @@ class UserDaoTest {
         Assertions.assertEquals("EternityHwan", selectedUser.getName());
         Assertions.assertEquals("1123", selectedUser.getPassword());
 
+    }
+
+    @Test
+    void count() throws SQLException, ClassNotFoundException {
+        UserDao userDao = new UserDaoFactory().awsUserDao();
+
+        User user1 = new User("1","박성철","1234");
+        User user2 = new User("2","이길원","1321");
+        User user3 = new User("3","박범진","4321");
+
+        userDao.deleteAll();
+        assertEquals(0,userDao.getCount());
+
+        userDao.add(user1);
+        assertEquals(1,userDao.getCount());
+
+        userDao.add(user2);
+        assertEquals(2,userDao.getCount());
+
+        userDao.add(user3);
+        assertEquals(3,userDao.getCount());
 
     }
 }
