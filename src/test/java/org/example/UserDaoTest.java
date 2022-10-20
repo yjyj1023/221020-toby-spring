d2ee342dae6ca473ac3bda17c104c7a69e9dafdc
yjyj1023 <lyj19961023@gmail.com>
2022-10-20 11:21:35 +0900

    toby spring복습

---
+++ src/test/java/org/example/UserDaoTest.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import user.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = UserDaoFactory.class)
+class UserDaoTest {
+
+    @Autowired
+    ApplicationContext context;
+
+    @Test
+    void addAndSelect() throws SQLException, ClassNotFoundException {
+        UserDao userDao = new UserDaoFactory().awsUserDao();
+        User user = new User("3", "EternityHwan","1123");
+        userDao.add(user);
+
+        User selectedUser = userDao.get("3");
+        Assertions.assertEquals("EternityHwan", selectedUser.getName());
+        Assertions.assertEquals("1123", selectedUser.getPassword());
+    }
+}
